33d6b57de0b544b8f44bf329e3b4924233937546
iEzCnF <alexcortes90@gmail.com>
2020-02-25 20:09:31 +0100

    Update linuxAIO.sh

--- linuxAIO.sh
+++ linuxAIO.sh
@@ -2,7 +2,7 @@
 echo ""
 echo "Welcome to NadekoBot. Downloading the latest installer..."
 root=$(pwd)
-wget -N https://github.com/Kwoth/NadekoBot-BashScript/raw/1.9/nadeko_master_installer.sh
+wget -N https://github.com/iEzCnF/NadekoBot-BashScript/raw/1.9/nadeko_master_installer.sh
 
 bash nadeko_master_installer.sh
 cd "$root"
